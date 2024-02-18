0f7dbecce52dda55d6ddd8a8535846ae30117ad6
Saddam Husein <saddambinhusein2@gmail.com>
2024-02-18 13:49:35 +0800

    02 Slicing Dashboard Company: Slicing post job page 1 - Creating form-schema and job types constants

---
+++ src/lib/form-schema.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { JOBTYPES } from '@/constants';
+import { z } from 'zod';
+
+export const jobFormSchema = z.object({
+  roles: z
+    .string({ required_error: 'Job Title is required' })
+    .min(3, { message: 'Job Title must be at least 3 characters' }),
+  jobType: z.enum(JOBTYPES, {
+    required_error: 'You need to select a job type',
+  }),
+  salaryFrom: z.string({ required_error: 'Salary Form is required' }),
+  salaryTo: z.string({ required_error: 'Salary To is required' }),
+  categoryId: z.string({ required_error: 'You need to select a category' }),
+  requiredSkills: z
+    .string()
+    .array()
+    .nonempty({ message: 'Required Skill must be at least 1 skill' }),
+  jobDescription: z
+    .string({ required_error: 'Job Description is required' })
+    .min(10, { message: 'Job Description must be at least 10 characters' }),
+  responsibility: z
+    .string({ required_error: 'Responsibility is required' })
+    .min(10, { message: 'Responsibility must be at least 10 characters' }),
+  whoYouAre: z
+    .string({ required_error: 'Who you are is required' })
+    .min(10, { message: 'Who you are must be at least 10 characters' }),
+  niceToHave: z
+    .string({ required_error: 'Nice to have is required' })
+    .min(10, { message: 'Nice to have must be at least 10 characters' }),
+  benefits: z
+    .object({
+      benefit: z.string(),
+      description: z.string(),
+    })
+    .array()
+    .nonempty({ message: 'Benefits must be at least 1 benefit' }),
+});
