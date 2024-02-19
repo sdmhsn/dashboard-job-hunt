014bcbc47f281e63355ed07eec152b6309efea46
Saddam Husein <saddambinhusein2@gmail.com>
2024-02-19 13:33:32 +0800

    02 Slicing Dashboard Company: Slicing post job page 2 - Creating form and input 1

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 import { jobFormSchema } from '@/lib/form-schema';
 import React, { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import { ArrowLeftIcon } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import FieldInput from '@/components/organisms/FieldInput';
+import { Input } from '@/components/ui/input';
 
 interface PostJobPageProps {}
 
@@ -18,7 +31,53 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
     console.log(val);
   };
 
-  return <div>PostJobPage</div>;
+  return (
+    <div>
+      <div className="inline-flex items-center gap-2 cursor-pointer hover:text-primary">
+        <ArrowLeftIcon className="w-7 h-7" />
+        <span className="text-2xl font-semibold">Post a Job</span>
+      </div>
+
+      <div className="my-5">
+        <div className="text-lg font-semibold">Basic Information</div>
+        <div className="text-gray-400">
+          List out your top perks and benefits.
+        </div>
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-5 space-y-6 pt-6"
+        >
+          {/* <FieldInput>: component in organisms that we created */}
+          <FieldInput
+            title="Job Title"
+            subtitle="Job title must be describe one position"
+          >
+            <FormField
+              control={form.control}
+              name="roles"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      className="w-[450px]"
+                      placeholder="e.g. Software Engineer"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default PostJobPage;
