38e2fc276bd54629b3b07fe95d8c1a9cfb11262a
Saddam Husein <saddambinhusein2@gmail.com>
2024-02-26 19:09:45 +0800

    02 Slicing Dashboard Company: Slicing settings overview component 2 - Creating Company Logo and Company Details input (Company Name, Company Website, Company Location) in OverviewForm component

--- src/components/forms/OverviewForm/index.tsx
+++ src/components/forms/OverviewForm/index.tsx
@@ -1,13 +1,31 @@
 'use client';
 
 import TitleForm from '@/components/atoms/TitleForm';
-import { Form } from '@/components/ui/form';
+import CustomUpload from '@/components/organisms/CustomUpload ';
+import FieldInput from '@/components/organisms/FieldInput';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Separator } from '@/components/ui/separator';
+import { Input } from '@/components/ui/input';
 import { overViewFormSchema } from '@/lib/form-schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import React, { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { LOCATION_OPTIONS, optionType } from '@/constants';
 
 interface OverviewFormProps {}
 
@@ -27,7 +45,88 @@ const OverviewForm: FC<OverviewFormProps> = ({}) => {
         subtitle={'This is company information that you can update anytime.'}
       />
 
-      <Separator />
+      <Separator className="mb-7" />
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-7">
+          <FieldInput
+            title="Company Logo"
+            subtitle="This image will be shown publicly as company logo."
+          >
+            <CustomUpload form={form} name="image" />
+          </FieldInput>
+
+          <FieldInput
+            title="Company Details"
+            subtitle="Introduce your company core info quickly to users by fill up company details."
+          >
+            <div className="space-y-5">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-[450px]"
+                        placeholder="My company"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="website"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Website</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-[450px]"
+                        placeholder="https://example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="location"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Location</FormLabel>
+                    <FormControl>
+                      <Select>
+                        <SelectTrigger className="w-[450px]">
+                          <SelectValue placeholder="Select location" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {LOCATION_OPTIONS.map(
+                            (item: optionType, i: number) => (
+                              <SelectItem key={item.id + i} value={item.id}>
+                                {item.label}
+                              </SelectItem>
+                            )
+                          )}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </FieldInput>
+        </form>
+      </Form>
     </div>
   );
 };
