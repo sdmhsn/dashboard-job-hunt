9b13de044408710ec8c50cc5d3d25964b0fc8477
Saddam Husein <saddambinhusein2@gmail.com>
2024-02-26 19:04:05 +0800

    02 Slicing Dashboard Company: Slicing settings overview component 2 - Creating optionType and LOCATION_OPTIONS constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,10 @@
 import { EnumValues } from 'zod';
 
+export type optionType = {
+  id: string;
+  label: string;
+};
+
 export const JOBTYPES: EnumValues = [
   'Full-Time',
   'Part-Time',
@@ -33,4 +38,23 @@ export const JOB_APPLICANTS_COLUMN: string[] = ['Roles', 'Applied Date'];
 
 export const JOB_APPLICANTS_DATA = [
   { name: 'Saddam', appliedDate: '15 Aug 2023' },
+];
+
+export const LOCATION_OPTIONS: optionType[] = [
+  {
+    id: 'Indonesia',
+    label: 'Indonesia',
+  },
+  {
+    id: 'Malaysia',
+    label: 'Malaysia',
+  },
+  {
+    id: 'Singapore',
+    label: 'Singapore',
+  },
+  {
+    id: 'Thailand',
+    label: 'Thailand',
+  },
 ];
