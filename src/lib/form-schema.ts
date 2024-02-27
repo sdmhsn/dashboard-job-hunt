6b841737d8da10915c2a77cda2b37b6ce7c76605
Saddam Husein <saddambinhusein2@gmail.com>
2024-02-27 18:49:43 +0800

    02 Slicing Dashboard Company: Slicing settings social links component - Creating socialMediaFormSchema in form-schema library

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -54,3 +54,11 @@ export const overViewFormSchema = z.object({
     .nonempty({ message: 'Tech stack must be at least 1 data' }),
   description: z.string({ required_error: 'Description is required' }),
 });
+
+export const socialMediaFormSchema = z.object({
+  facebook: z.string({ required_error: 'Facebook link is required' }),
+  instagram: z.string({ required_error: 'Instagram link is required' }),
+  linkedin: z.string({ required_error: 'LinkedIn link is required' }),
+  twitter: z.string({ required_error: 'Twitter link is required' }),
+  youtube: z.string({ required_error: 'Youtube link is required' }),
+});
