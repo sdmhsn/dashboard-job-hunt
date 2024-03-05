8bcda929b4750a2f5a104d2802a16bf38534a428
Saddam Husein <saddambinhusein2@gmail.com>
2024-03-05 13:17:48 +0800

    03 Integration Dashboard Company: Integration Sign up - Using hashPassword util in company/new-user API 2

--- src/app/api/company/new-user/route.tsx
+++ src/app/api/company/new-user/route.tsx
@@ -1,15 +1,17 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../../../lib/prisma';
+import { hashPassword } from '@/lib/utils';
 
 export const dynamic = 'force-dynamic'; // defaults to auto
 export async function POST(request: Request) {
   const data = await request.json();
+  const hashedPassword = await hashPassword(data.password);
 
   const result = await prisma.company.create({
     data: {
       name: data.name,
       email: data.email,
-      password: data.password,
+      password: hashedPassword,
     },
   });
 
