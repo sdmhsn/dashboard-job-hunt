5a34b0452711d86e981924465634651fe71be598
Saddam Husein <saddambinhusein2@gmail.com>
2024-03-09 14:56:02 +0800

    03 Integration Dashboard Company: Integration Sign In 2 - Disable the signup and signin page after user signed in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,23 @@
 import { Epilogue } from 'next/font/google';
 import '../globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 const epilogue = Epilogue({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
+  if (session !== null) {
+    return redirect('/');
+  }
+
   return (
     <html lang="en">
       <body className={epilogue.className}>
